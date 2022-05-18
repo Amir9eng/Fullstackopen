f2a9960c52ea02d8d8b4ad0ee26f066e12430bf6
Amir9eng <amirmukhtarme@gmail.com>
2022-05-18 14:13:45 +0100

    Data for Countries step2

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -3,9 +3,10 @@ import axios from "axios";
 
     
     const SingleCountry = ({country}) => {
-    const {name:{common}, capital, area, population, languages, flags} = country[0]
+      console.log(country);
+    const {name: {common}, capital, area, population, languages, flags} = country[0]
     return (
-      <div>
+      <div>       
         <h2>{common}</h2>
         <p>Capital - {capital}</p>
         <p>Area - {area}</p>
@@ -19,6 +20,7 @@ import axios from "axios";
 
 
   const DisplayCountries = ({ search, countries }) => {
+    const [showCountries, setShowCountries] = useState([])
     const countriesToShow = search.trim()
       ? countries.filter((country) =>  country.name.common.toLowerCase().includes(search.toLowerCase())
     
@@ -28,15 +30,21 @@ import axios from "axios";
   
     return (
       <div>
-        {countriesLength === 1 ? (
+        { !showCountries.length ? 
+        countriesLength === 1 ? (
           <SingleCountry country={countriesToShow} />
         ) : countriesLength < 10 ? (
           countriesToShow.map((country, index) => (
+            <div style={{display: 'flex', alignItems: 'center'}} key={index}>
             <p key={index}>{country.name.common}</p>
+            <button onClick={(e) => setShowCountries([country])}>show</button>
+            </div>
           ))
         ) : (
           <p>Too many matches, specify another filter </p>
-        )}
+        ) : 
+        <SingleCountry country={showCountries} />
+      }
       </div>
     );
   };
@@ -45,14 +53,17 @@ import axios from "axios";
 function App() {
   const [countries, setCountries] = useState([])
   const [search, setSearch] = useState("")
+  console.log(countries);
   useEffect(() => {
     axios.get('https://restcountries.com/v3.1/all').then(response => {
+      console.log("gggg")
       setCountries(response.data)
-    })
+      console.log(response.data);
+    }).catch((err)=>console.log(err))
   }, [])
 
   return <div className="App">
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form onSubmit={(e) => e.preventDefault()}> 
       {countries.length? <div>
       find countries: <input type="text" value={search} onChange={(e) => setSearch(e.target.value)}/>
       </div> : <span>Loading...</span>}
