e42d78888c53784d314f5fc653696f8be24cb5a2
Amir9eng <amirmukhtarme@gmail.com>
2022-06-29 23:41:02 +0100

    exercise 7.4

--- part7/routed-anecdotes/src/components/CreateNew.js
+++ part7/routed-anecdotes/src/components/CreateNew.js
@@ -1,16 +1,16 @@
-import { useState } from "react";
+import { useField } from "../hooks";
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const content = useField("text")
+  const author = useField('text')
+  const info= useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
   };
@@ -22,25 +22,19 @@ const CreateNew = (props) => {
         <div>
           content
           <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            {...content}
           />
         </div>
         <div>
           author
           <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
+          {...author}
           />
         </div>
         <div>
           url for more info
           <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
+            {...info}
           />
         </div>
         <button>create</button>
